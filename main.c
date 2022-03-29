ca7e5e141048d46481c37ce5d26ea9f6cb7e0b55
Ziggy <davidarogers@protonmail.com>
2022-03-29 18:39:10 +0000

    command line frequency arguments

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 void white_noise(Sint16 *stream, size_t stream_len) {
   for (size_t i = 0; i < stream_len; i++) {
-    Sint16 value = rand() % (1 << 15);
+    Sint16 value = rand() % (1 << 10);
     Sint16 sign = (rand() % 2) * 2 - 1;
     stream[i] = value * sign;
   }
@@ -19,13 +19,16 @@ void my_beep_callback(void *userdata, Uint8 *stream, int len) {
   white_noise((Sint16 *)stream, len / 2);
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  assert(argc > 1);
+  int cl_freq_param = atoi(argv[1]);
+
   if (SDL_Init(SDL_INIT_AUDIO) < 0) {
     fprintf(stderr, "ERROR: could not initialize SDL: %s\n", SDL_GetError());
   }
 
   SDL_AudioSpec desired = {
-      .freq = 40000,
+      .freq = cl_freq_param,
       .format = AUDIO_S16LSB,
       .channels = 1,
       .callback = my_beep_callback,
